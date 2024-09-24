f3bfd79edabbca8d09dbefdf62ae9958bf24e182
Geoffry Song <goffrie@gmail.com>
2024-09-23 18:32:43 -0700

    add eslint-plugin-react-hooks

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,21 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import pluginReact from 'eslint-plugin-react';
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import pluginReactHooks from "eslint-plugin-react-hooks";
 
 export default [
-    { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+    { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
     { languageOptions: { globals: globals.browser } },
-    { ignores: ['node_modules/', 'dist/'] },
+    { ignores: ["node_modules/", "dist/"] },
     pluginJs.configs.recommended,
     pluginReact.configs.flat.recommended,
+    {
+        plugins: {
+            "react-hooks": pluginReactHooks,
+        },
+        rules: pluginReactHooks.configs.recommended.rules,
+    },
     ...tseslint.configs.recommended,
-    { rules: { 'react/react-in-jsx-scope': 'off' } },
+    { rules: { "react/react-in-jsx-scope": "off" } },
 ];
