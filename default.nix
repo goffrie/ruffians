9b2930c845a1a32616a2e3244dcc72552acb76ff
Geoffry Song <goffrie@gmail.com>
2024-09-24 01:59:14 +0000

    Fix nix build

--- default.nix
+++ default.nix
@@ -7,21 +7,10 @@ pkgs.mkYarnPackage {
   yarnLock = ./yarn.lock;
   nativeBuildInputs = [ pkgs.jq ];
   buildPhase = ''
-    mkdir deps/ruffians/node_modules_2
-    ln -s $(realpath node_modules)/{*,.bin} deps/ruffians/node_modules_2/
-    rm deps/ruffians/node_modules
-    mv deps/ruffians/node_modules_2 deps/ruffians/node_modules
-
-    yarn build
+    yarn build --dist-dir $out
   '';
   installPhase = ''
-    mkdir $out
-    cp -r deps/ruffians/build/{index.html,favicon*.*,robots.txt,static} $out/
-    for map in deps/ruffians/build/static/js/*.map; do
-      jq -c '.sources |= map (sub(".*/node_modules"; "node_modules"))' < $map > $out/static/js/$(basename $map)
-    done
+    cp -r deps/ruffians/src/robots.txt $out/
   '';
-  distPhase = ''
-    true
-  '';
+  doDist = false;
 }
--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,3 +1,5 @@
+import "@types/webpack-env"; // for `module.hot`
+
 declare const process: {
     env: {
         NODE_ENV: string;
